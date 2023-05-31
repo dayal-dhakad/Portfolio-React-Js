41cefe1c07cbff54ee3504852aa0fee4bd8a55da
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-05-31 19:03:07 +0530

    projects section completed

---
+++ src/components/Work.js
@@ -0,0 +1,22 @@
+import './WorkCardStyles.css'
+import React from 'react'
+import WorkCard from './WorkCard'
+import WorkCardData from './WorkCardData'
+import pro1 from '../assets/project1.png'
+import { NavLink } from 'react-router-dom'
+const Work = () => {
+  return (
+    <div className="work-container">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
+          {WorkCardData.map((val, ind)=>{
+            return (
+                <WorkCard key={ind} imgsrc={val.imgsrc} title={val.title} text={val.text} view={val.view} />
+            )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Work
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,28 @@
+import pro1 from '../assets/project1.png'
+import pro2 from '../assets/project2.png'
+import pro3 from '../assets/project3.png'
+
+const ProjectCardData=[
+    {
+        imgsrc: pro1,
+        title: "Supermarket Billing System",
+        text: "It is using to make bills in supermarkets",
+        view: "https://github.com/dayal-dhakad/C-Project-Supermarket-Billing-System-"
+    },
+
+    {
+        imgsrc: pro2,
+        title: "Flat Rental System",
+        text: "A Flat Rental System project using html css javascript",
+        view: "https://dayal-dhakad.github.io/Flat-Rental-System/"
+    },
+
+    {
+        imgsrc: pro3,
+        title: "NewsApp",
+        text: "The goal is to create a News Feed app which gives a user regularly-updated news from the internet related to a particular topic. In this project, use API from newsapi.org . This is a well-maintained API which returns information in a JSON format.",
+        view: "https://github.com/dayal-dhakad/ReactJs-Newsapp-"
+    }
+]
+
+export default ProjectCardData;
